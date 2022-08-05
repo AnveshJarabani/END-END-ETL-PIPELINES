9b3aa794023596a0c1d23920a16937b769b66264
Anvesh Jarabani <AJarabani@uct.com>
2022-08-05 14:23:57 -0700

    first commit

--- ARCHIVE/FILTER PH.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-LVLBOMS_PATH = r"C:\Users\ajarabani\Desktop\Parts\DATA ANALYSIS\COSTING ANALYSIS\DASHBOARD DATA\LAM_LVLBOMS.PKL"
-PH_PATH = r"C:\Users\ajarabani\Desktop\Parts\DATA ANALYSIS\COSTING ANALYSIS\DASHBOARD DATA\Purchase History Report_R2R-PCO-008 (Q1, Q2 2022).xlsx"
-PH = pd.read_excel(PH_PATH,sheet_name=1,usecols='C',skiprows=[0])
-PHA = PH.iloc[:,0].values
-LVLBOMS = pd.read_pickle(LVLBOMS_PATH)
-LVLBOMS = LVLBOMS.loc[~LVLBOMS['TOPLEVEL'].isin(PHA)]
-LVLBOMS.to_pickle(LVLBOMS_PATH)
--- ARCHIVE/READ PICKLE.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import xlwings as xl
-LVLBOMS_PATH = r"C:\Users\ajarabani\Desktop\Parts\DATA ANALYSIS\COSTING ANALYSIS\DASHBOARD DATA\LAM_LVLBOMS.PKL"
-lvlboms = pd.read_pickle(LVLBOMS_PATH)
-ws = xl.books.active.sheets.active
-ws.range('A2').options(index=False).value=lvlboms
