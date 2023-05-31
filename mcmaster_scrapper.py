ccda75bc12f0ef4e0ecca9d7da1c50aded5b9484
anveshjarabani <anveshjarabani@gmail.com>
2023-05-31 15:19:03 -0700

    updates

--- mcmaster_scrapper.py
+++ mcmaster_scrapper.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import multiprocessing as mp
-import warnings,os,time
+import warnings,os,time,re
 warnings.simplefilter(action='ignore', category=(FutureWarning, UserWarning))
 os.environ['PYDEVD_DISABLE_FILE_VALIDATION'] = '1'
 chromeOptions = webdriver.ChromeOptions()
@@ -30,8 +30,8 @@ def cost_extractor(i):
             print(x)
             dct = {'link': [lnk],
                    'PN': [i],
-                   'COST': [x],
-                   'TIER': ['STD']}
+                   '$$': [x],
+                   'T': None}
             return pd.DataFrame(dct)
             
     except:
@@ -42,24 +42,29 @@ def cost_extractor(i):
             if m==1:
                 dct = {'link': [lnk],
                        'PN': [i],
-                       'COST': [lst[1].text],
-                       'TIER': [lst[0].text]}
+                       '$$': [lst[1].text],
+                       'T': [lst[0].text]}
                 
             else:
                 dct['link'].append(lnk)
                 dct['PN'].append(i)
-                dct['COST'].append(lst[1].text)
-                dct['TIER'].append(lst[0].text)
+                dct['$$'].append(lst[1].text)
+                dct['T'].append(lst[0].text)
             m+=1
             print(lst[0].text,lst[1].text)
         return pd.DataFrame(dct)
 if __name__ == '__main__':
-    pool = mp.Pool(processes=mp.cpu_count())
+    pool = mp.Pool(processes=6)
     results=pool.map(cost_extractor, MPN_df.iloc[:,0].values)
-    res_df=pd.concat(results)
-    res_df['$$'] = res_df['COST'].str.extract('(\$\d+\.\d+)')
-    res_df=res_df.sort_values(['PN','$$'])
-    xl.books.active.sheets(3).range('N1').options(index=False).value=res_df
+    res_df=pd.concat(results,ignore_index=True)
+    res_df['COST'] = res_df['$$'].str.extract('(\$\d+\.\d+)')
+    res_df=res_df.sort_values(['PN','COST'],ascending=[True,False])
+    res_df['TIER']=res_df['$$'].apply(lambda x:'EA' if 'Each' in x else 
+                                      '{} Pack'.format(re.search('\d+$', x)[0]))
+    res_df.loc[res_df['T'].notna(),'TIER']=res_df['T']
+    res_df=res_df[['link','PN','TIER','COST']]
+    xl.books.active.sheets.add()
+    xl.books.active.sheets.active.range('A1').options(index=False).value=res_df
     print('processed in {} secs'.format(round(time.time()-start,2)))
 
 
