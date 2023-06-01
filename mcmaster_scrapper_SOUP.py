5f88408a6ee4c794c02e8b71dd36e0276de89162
anveshjarabani <anveshjarabani@gmail.com>
2023-06-01 10:05:39 -0700

    updates

--- mcmaster_scrapper_SOUP.py
+++ mcmaster_scrapper_SOUP.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import multiprocessing as mp
 import warnings,os,time,re
+from bs4 import BeautifulSoup as bs
 warnings.simplefilter(action='ignore', category=(FutureWarning, UserWarning))
 os.environ['PYDEVD_DISABLE_FILE_VALIDATION'] = '1'
 chromeOptions = webdriver.ChromeOptions()
@@ -23,34 +24,35 @@ def cost_extractor(i):
     WebDriverWait(driver, 20).until(EC.any_of(
         EC.text_to_be_present_in_element(
         (By.CSS_SELECTOR, "[class*='PrceTxt']"),'$'),
-        EC.presence_of_element_located(
-        (By.CSS_SELECTOR,"[data-mcm-prce-lvl]"))
+        EC.text_to_be_present_in_element(
+            (By.CSS_SELECTOR, "[data-mcm-prce-lvl]"),)
         ))
-    try:
-        element=driver.find_element(By.CSS_SELECTOR, "[class*='PrceTxt']")
-        if element:
-            x=element.text
-            print(f'\033[93m{x}\033[0m')
-            dct = {'link': [lnk],
-                   'PN': [i],
-                   '$$': [x],
-                   'T': None}
-            return pd.DataFrame(dct)      
-    except:
+    source=driver.page_source
+    soup=bs(source,'html.parser')
+    element=soup.find('div', class_='PrceTxt')
+    if element:
+        x=element.text
+        print(x)
+        dct = {'link': [lnk],
+                'PN': [i],
+                '$$': [x],
+                'T': None}
+        return pd.DataFrame(dct)
+        
+    else:
         m=1
         dct={'link':[],
                 'PN':[],
                 '$$':[],
                 'T':[]}
-        while driver.find_elements(By.CSS_SELECTOR,"[data-mcm-prce-lvl='{}']".format(m)):
-            lst = driver.find_elements(
-                By.CSS_SELECTOR, "[data-mcm-prce-lvl='{}']".format(m))
+        while soup.find_all("[data-mcm-prce-lvl='{}']".format(m)):
+            lst = soup.find_all("[data-mcm-prce-lvl='{}']".format(m))
             dct['link'].append(lnk)
             dct['PN'].append(i)
             dct['$$'].append(lst[1].text)
             dct['T'].append(lst[0].text)
             m+=1
-            print(f'\033[96m{lst[0].text,lst[1].text}\033[0m')
+            print(lst[0].text,lst[1].text)
         return pd.DataFrame(dct)
 if __name__ == '__main__':
     pool = mp.Pool(processes=8)
