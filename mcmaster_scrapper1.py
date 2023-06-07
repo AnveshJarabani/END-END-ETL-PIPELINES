3032d2d26d53f9d3dc8b63e6028c2875a9ce1d1e
anveshjarabani <anveshjarabani@gmail.com>
2023-06-07 11:39:10 -0700

    'updates'

--- mcmaster_scrapper1.py
+++ mcmaster_scrapper1.py
@@ -22,20 +22,9 @@ if __name__ == '__main__':
     MPN_df=xl.books.active.sheets(2).range('A1').expand('down').options(index=False).options(pd.DataFrame,index=False).value
     home_raw = 'https://www.mcmaster.com/{}/'
     driver = webdriver.Chrome('sldr.exe',options=chromeOptions,desired_capabilities=caps)
+    test_raw = 'https://www.mcmaster.com/WebParts/Ordering/InLnOrdWebPart/ItmPrsnttnDynamicDat.aspx?acttxt=dynamicdat&partnbrtxt={}'
     driver.get(home_raw.format(MPN_df.iloc[0,0]))
     time.sleep(2)
-    browser_log=driver.get_log('performance')
-    events=[log_entry(entry) for entry in browser_log]
-    events=[event for event in events if 'Network.response' in event['method']]
-    for i in events:
-        if 'params' in i.keys():
-            if 'response' in i['params']:
-                if 'aspx' in i['params']['response']['url']:
-                    t = re.search(r"\/(\w+)\/",i['params']['response']['url']).group(1)
-                    break
-    raw = f"https://www.mcmaster.com/{t}/WebParts/Ordering/InLnOrdWebPart/ItmPrsnttnDynamicDat.aspx?"+ \
-        "acttxt=getstockstatus&partnbrtxt={}&possiblecompnbrtxt=&isinlnspec=false&"+ \
-        "attrCompIds=&attrnm=&attrval=&cssAlias=undefined&useEs6=true"
     all_cookies=driver.get_cookies()
     cookies={}
     for i in all_cookies:
@@ -43,7 +32,7 @@ if __name__ == '__main__':
     driver.close()
     start=time.time()
     pool = mp.Pool(processes=10)
-    results=pool.starmap(cost_extractor, [(i,raw,cookies) for i in MPN_df.iloc[:,0]])
+    results=pool.starmap(cost_extractor, [(i,test_raw,cookies) for i in MPN_df.iloc[:,0]])
     res_df=pd.concat([pd.DataFrame(i) for i in results],ignore_index=True)
     res_df['COST'] = res_df['$$'].str.extract('(\$\d+\.\d+)')
     res_df=res_df.sort_values(['PN','COST'],ascending=[True,False],ignore_index=True)
@@ -56,3 +45,16 @@ if __name__ == '__main__':
     print('processed in {} secs'.format(round(time.time()-start,2)))
     xl.books.active.sheets.active.range('N1').options(index=False).value=res_df
     print(res_df)
+    
+    # browser_log=driver.get_log('performance')
+    # events=[log_entry(entry) for entry in browser_log]
+    # events=[event for event in events if 'Network.response' in event['method']]
+    # for i in events:
+    #     if 'params' in i.keys():
+    #         if 'response' in i['params']:
+    #             if 'aspx' in i['params']['response']['url']:
+    #                 t = re.search(r"\/(\w+)\/",i['params']['response']['url']).group(1)
+    #                 break
+    # raw = f"https://www.mcmaster.com/{t}/WebParts/Ordering/InLnOrdWebPart/ItmPrsnttnDynamicDat.aspx?"+ \
+    #     "acttxt=dynamicdat&partnbrtxt={}&possiblecompnbrtxt=&isinlnspec=false&"+ \
+    #     "attrCompIds=&attrnm=&attrval=&cssAlias=undefined&useEs6=true"
