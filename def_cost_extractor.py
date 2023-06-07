e9002702fbc20f2e144a79af6faa2089422a723c
anveshjarabani <anveshjarabani@gmail.com>
2023-06-07 11:39:10 -0700

    'updates'

--- def_cost_extractor.py
+++ def_cost_extractor.py
@@ -1,17 +1,17 @@
 import requests,json
 def cost_extractor(i, raw, cookies):
-    lnk = raw.format(i)
-    dict = json.loads(requests.get(lnk, cookies=cookies).text)
+    dict = json.loads(requests.get(raw.format(i), cookies=cookies).text)
     if not dict['PrceTiers']:
         # print(dict['PrceTxt'])
         return {'PN': [i],
                 '$$': [dict['PrceTxt']],
                 'T': None}
     else:
-        return {'PN': [i]*len(dict['PrceTiers']),
-                '$$': [i['PrceTierPrceTxt']
-                       for i in dict['PrceTiers']],
-                'T': [i['PrceTierQtyTxt']
-                      for i in dict['PrceTiers']]}
+        dct=dict['PrceTiers']
         # print([(i['PrceTierPrceTxt'], i['PrceTierQtyTxt'])
             #    for i in dict['PrceTiers']])
+        return {'PN': [i]*len(dct),
+                '$$': [i['PrceTierPrceTxt']
+                       for i in dct],
+                'T': [i['PrceTierQtyTxt']
+                      for i in dct]}
