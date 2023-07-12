1f2feef00a285a94880ae5d0b3cfb735e3a59677
anveshjarabani <anveshjarabani@gmail.com>
2023-07-12 10:30:37 -0700

    updates

--- BI_PULL/ALL_BI_PULL.py
+++ BI_PULL/ALL_BI_PULL.py
@@ -1,5 +1,10 @@
 import subprocess,glob
 scripts=glob.glob("*.py")
 BI_PULLS=[i for i in scripts if 'ALL' not in i and "DB" not in i]
+BI_TO_DB=[i for i in scripts if 'DB' in i]
 for i in BI_PULLS:
     subprocess.run(["python",i],stderr=subprocess.DEVNULL)
+for i in BI_TO_DB:
+    subprocess.run(["python",i],stderr=subprocess.DEVNULL)
+    
+
