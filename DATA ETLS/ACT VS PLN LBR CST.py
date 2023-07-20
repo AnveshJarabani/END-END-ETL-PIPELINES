34dd31c26f4db1ba5c1731dac86a280f80904032
anveshjarabani <anveshjarabani@gmail.com>
2023-07-19 17:02:00 -0700

    updates

--- DATA ETLS/ACT VS PLN LBR CST.py
+++ DATA ETLS/ACT VS PLN LBR CST.py
@@ -3,7 +3,7 @@ import numpy as np
 wl=pd.read_pickle('../PKL/RAW_LBR.PKL')
 rout=pd.read_hdf('../H5/ST_BM_BR.H5',key='ROUT')
 SM=pd.DataFrame()
-SM['SM']=np.array(rout.loc[rout['WORK_CENTER'].str.contains(r'BRAKE|LASER|TRT'),"Material"].unique()).tolist()
+SM['SM']=np.array(rout.loc[rout['Work Center'].str.contains(r'BRAKE|LASER|TRT'),"Material"].unique()).tolist()
 SM.to_hdf('../H5/ST_BM_BR.H5',key='SM_PARTS',mode='a')
 cst = pd.read_hdf('../H5/ST_BM_BR.H5',key='BR')
 wl = wl[(wl['PART_NUMBER'] != '#') & (wl['WORK_ORDER'] != '#') & (wl['STD_KEY'] != '#')]
@@ -34,7 +34,7 @@ def remove_outliers(group):
     lower_bound = Q1 - 0.5*IQR
     return group[(group > lower_bound) & \
                   (group < upper_bound)]
-pi[['ACT COST/EA_t', 'HRS/EA_t']] = pi.groupby(['PART_NUMBER', 'STD_KEY'])[['ACT COST/EA', 'HRS/EA']].transform(remove_outliers)
+# pi[['ACT COST/EA_t', 'HRS/EA_t']] = pi.groupby(['PART_NUMBER', 'STD_KEY'])[['ACT COST/EA', 'HRS/EA']].transform(remove_outliers)
 pi_temp = pi.pivot_table(index=["PART_NUMBER",'STD_KEY'],values = ['ACT COST/EA'],aggfunc= np.mean)
 pi_temp.reset_index(inplace=True)
 pi_temp.columns=['MAT_TEMP','STKEY_TEMP','COST/EA_AVG']
@@ -43,7 +43,8 @@ pi=pi.loc[(pi['COST/EA_AVG']/5 < pi['ACT COST/EA']) & (pi['COST/EA_AVG']*5 > pi[
 pi=pi.pivot_table(index=["PART_NUMBER",'STD_KEY'],values = ['HRS/EA','ACT COST/EA'],aggfunc= np.mean)
 pi.reset_index(inplace=True)
 rout.columns=rout.columns.str.strip()
-rt=rout.loc[:,['Material','STD_KEY','Base Quantity','WORK_CENTER','Setup','Unit_Setup','Labor Run','Unit_Labor Run']]
+rout.columns
+rt=rout.loc[:,['Material','STD_KEY','Base Quantity','Work Center','Setup','Unit_Setup','Labor Run','Unit_Labor Run']]
 rt=rt.loc[rout['Unit_Setup']!='#']
 QTY=wl[['PART_NUMBER','WORK_ORDER','OP_QTY']]
 QTY=QTY.drop_duplicates()
@@ -51,7 +52,7 @@ QTY = QTY.pivot_table(index=['PART_NUMBER'],values=['OP_QTY'],aggfunc=np.mean)
 QTY.reset_index(inplace=True) 
 rt = rt.merge(QTY,left_on='Material',right_on='PART_NUMBER',how='left')
 rt.drop(columns=['PART_NUMBER'],axis=1,inplace=True)
-rt.loc[(rt['OP_QTY'].isnull())&(rt['WORK_CENTER'].str.contains(r'BRAKE|LASER|TRT|SAW')),'Base Quantity'] = 10
+rt.loc[(rt['OP_QTY'].isnull())&(rt['Work Center'].str.contains(r'BRAKE|LASER|TRT|SAW')),'Base Quantity'] = 10
 rt.loc[(rt['OP_QTY']!=1),'Base Quantity']=10
 for i in rt.loc[rt['Base Quantity']==10,'Material'].unique():
      rt.loc[rt['Material']==i,'Base Quantity']=10
@@ -59,10 +60,10 @@ rt.loc[rt['OP_QTY'].isnull(),'OP_QTY'] = rt['Base Quantity']
 rt['PLN HR/EA'] = ((rt['Setup'] + rt['Labor Run'])/rt['Base Quantity']).where(rt['Unit_Labor Run'] == 'H',\
                  (rt['Setup'] + (rt['Labor Run']/60))/rt['Base Quantity'])
 rt = rt.pivot_table(index=['Material','STD_KEY'],values=['PLN HR/EA'],aggfunc=np.sum)
-rt.reset_index(inplace=True)
+rt.reset_index(inplace=True) 
 pi3 = pi.pivot_table(index=["PART_NUMBER"],values=['ACT COST/EA','HRS/EA'],aggfunc= np.sum)
 pi3.reset_index(inplace=True)
-rt = rt.merge(cst,left_on='STD_KEY',right_on='ST KEY',how='left')
+rt = rt.merge(cst,left_on='Standard Text Key',right_on='ST KEY',how='left')
 rt.drop(columns=['ST KEY'],axis=1,inplace=True)
 rt['PLN COST/EA'] = rt['PLN HR/EA']*rt['BUR_RATE']
 rt = rt.pivot_table(index=['Material'],values=['PLN COST/EA'],aggfunc=np.sum)
