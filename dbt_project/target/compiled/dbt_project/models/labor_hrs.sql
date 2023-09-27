8bc94d7170f67e6ff77b7e9a317a733fb977d0cd
Anvesh Jarabani <anveshjarabani@gmail.com>
2023-09-26 19:04:29 -0700

    updates

---
+++ dbt_project/target/compiled/dbt_project/models/labor_hrs.sql
@@ -0,0 +1,7 @@
+-- Active: 1688765630308@@localhost@5432@uct_data@public
+SELECT
+    *
+FROM
+    raw_lbr
+WHERE
+    "PART_NUMBER" != '#'
---
+++ dbt_project/target/run/dbt_project/models/labor_hrs.sql
@@ -0,0 +1,13 @@
+
+  create view "uct_data"."public"."labor_hrs__dbt_tmp"
+    
+    
+  as (
+    -- Active: 1688765630308@@localhost@5432@uct_data@public
+SELECT
+    *
+FROM
+    raw_lbr
+WHERE
+    "PART_NUMBER" != '#'
+  );
